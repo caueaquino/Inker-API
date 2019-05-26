6b7696db9e507dc8234c7af3171120c0adfb9504
Cauê Forlogic <caue@forlogic.net>
2019-05-26 16:26:33 -0300

    update screen

--- inker/src/components/Login/index.js
+++ inker/src/components/Login/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 
 import './styles.css';
 
+import logo from '../../assets/images/Logo.png';
+
 import TextField from '@material-ui/core/TextField';
 import { Fab } from '@material-ui/core';
 
@@ -9,43 +11,62 @@ export default class index extends Component {
     render() {
         return (
             <section id="login-container">
-                <h1>LOGIN</h1>
+                
+                <img 
+                    width="100"
+                    src={{logo}} 
+                    alt="logo inker"
+                >
+                </img>
+                
+                <h1>
+                    LOGIN
+                </h1>
+                
                 <TextField
                     required
-                    id="standard-required"
+                    id="email-input"
                     label="E-mail"
-                    style={{marginTop: 30}}
+                    style={{marginTop: 40, width: 250}}
                     margin="normal"
                     fullWidth
                 />
+                
                 <TextField
                     required
-                    id="standard-password-input"
+                    id="password-input"
                     label="Senha"
+                    style={{marginTop: 30}}
                     type="password"
                     autoComplete="current-password"
                     margin="normal"
                     fullWidth
                 />
+                
                 <Fab
                     variant="extended"
                     size="large"
-                    style={{marginTop: 30}}
-                    color="white"
+                    style={{marginTop: 40}}
                     id="bt-login"
                 >
                     Entrar
                 </Fab>
+                
                 <Fab
                     variant="extended"
                     size="large"
-                    style={{marginTop: 10}}
-                    color="white"
+                    style={{marginTop: 20}}
                     id="bt-register"
                 >
                     Cadastrar-se
                 </Fab>
-                <a></a>
+                
+                <button 
+                    id="bt-recover"
+                >
+                    Esqueceu a senha?
+                </button>
+            
             </section>
         )
     }
